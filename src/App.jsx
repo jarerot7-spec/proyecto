af889a676b3300daa0a73b779857b504f1afb04c
faze-gf <gccotarma.com>
2026-06-30 19:26:51 -0500

    Actualizo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Profile from "./components/Profile";
 
 function App() {
-  return <Profile />;
+  return (
+    <Routes>
+      {/* Página principal */}
+      <Route path="/" element={<Login />} />
+
+      {/* Registro */}
+      <Route path="/register" element={<Register />} />
+
+      {/* Perfil */}
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
