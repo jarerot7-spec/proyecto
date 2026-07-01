f4e13df9486f76a71686b61f42139b7a8abd8470
jarerot7-spec <jarerot7@gmail.com>
2026-07-01 16:05:05 -0500

    Update productos.js

--- src/components/data/productos.js
+++ src/components/data/productos.js
@@ -53,5 +53,3 @@ export const productos = [
         imagen: "https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ]
-
-export default productos;
